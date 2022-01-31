830ece448e3a10389ea90fc82e20120c91b3a062
kfortaleza <kfortaleza@uolinc.com>
2022-01-30 21:24:01 -0300

    Criação de nova classe

--- src/ImcApp.java
+++ src/ImcApp.java
@@ -1,37 +1,10 @@
-import java.util.Scanner;
 
 public class ImcApp {
 
 	public static void main(String[] args) {
 			
-		// Declaração de Variáveis
-		String nomeDoUsuario;
-		int pesoDoUsuario;
-		double alturaDoUsuario, valorImc;
-		Imc imc = new Imc();
-		
-		// Ler o teclado do usuário
-		Scanner leitor = new Scanner(System.in);
-		
-		// Início
-		System.out.println("Qual o seu nome? ");
-		nomeDoUsuario = leitor.next();
-	
-		System.out.print(nomeDoUsuario + ", digite seu peso:\n");
-		pesoDoUsuario = leitor.nextInt();		
-		
-		System.out.print("E a sua altura:\n");
-		alturaDoUsuario = leitor.nextDouble();
-		
-		leitor.close();
-		
-		//Calculando utilizando a classe Imc
-		valorImc = 	imc.calcularImc(pesoDoUsuario, alturaDoUsuario);
-		System.out.println(nomeDoUsuario + ", seu IMC é " + valorImc + ".");
-		
-		System.out.println("-------------------------------------------");
-		
-		imc.classificarImc(valorImc);
+		Resultado r = new Resultado();
+		r.executar();
 
 	} 
 
